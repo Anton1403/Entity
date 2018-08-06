a600b8e412427e46ae6262456a3bb354716418ac
Anton Bialenik <tony.baton@yandex.by>
2018-08-06 14:42:29 +0300

    First commit

---
+++ Entity/Program.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.SqlServer;
+
+namespace Entity
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    class UserContext : DbContext
+    {
+        public UserContext()
+            :base("DbConnection")
+        { }
+
+        public DbSet<User> Users { get; set; }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            using (UserContext db = new UserContext())
+            {
+                User user1 = new User { Name = "Tom", Age = 33 };
+                User user2 = new User { Name = "Sam", Age = 26 };
+                db.Users.Add(user1);
+                db.Users.Add(user2);
+                db.SaveChanges();
+                Console.WriteLine("Objects successfully saved");
+                var users = db.Users;
+                Console.WriteLine("List of objects: ");
+                foreach (User u in users)
+                {
+                    Console.WriteLine("{0}.{1} - {2}", u.Id, u.Name, u.Age);
+                }
+            }
+            Console.Read();
+        }
+    }
+}
